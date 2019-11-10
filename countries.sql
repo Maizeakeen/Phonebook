27f6df270af7c14d687ef5fd9daa7af4a68ac261
Vladisalav <vladsaenko2197@gmail.com>
2019-11-10 12:52:03 +0300

    Дамб бд

---
+++ countries.sql
@@ -0,0 +1,167 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Ноя 09 2019 г., 17:13
+-- Версия сервера: 8.0.15
+-- Версия PHP: 7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `countries`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `countries`
+--
+
+CREATE TABLE `countries` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `countries`
+--
+
+INSERT INTO `countries` (`id`, `name`) VALUES
+(1, 'Ukrain'),
+(2, 'Poland'),
+(3, 'America'),
+(4, 'Arctica'),
+(5, 'England'),
+(6, 'German'),
+(7, 'France'),
+(8, 'Italia'),
+(9, 'Danmark');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `email`
+--
+
+CREATE TABLE `email` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `flag` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `email`
+--
+
+INSERT INTO `email` (`id`, `email`, `flag`) VALUES
+(3, 'asdas', 1),
+(2, 'mail', 1),
+(1, 'email', 1),
+(1, 'emailemail', 1),
+(1, 'asdfsdf', 1),
+(1, 'aasdas', NULL),
+(1, 'asdasdas', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `phone`
+--
+
+CREATE TABLE `phone` (
+  `id` int(11) NOT NULL,
+  `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `flag` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `phone`
+--
+
+INSERT INTO `phone` (`id`, `phone`, `flag`) VALUES
+(3, '123123', 1),
+(2, '123346', 1),
+(1, '12345', 1),
+(1, '67890', NULL),
+(1, '1234353245', NULL),
+(1, '123434', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL,
+  `street` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `password`, `name`, `lastname`, `city`, `country`, `street`) VALUES
+(1, 'john', '12345', 'Victor', 'Smith', 'Dnepr', 'Ukrain', '123, sreet'),
+(2, 'james', '54321', 'Sasha', 'Bald', 'New York', 'America', '123, sreet'),
+(3, 'rick', 'password', 'Rick', 'Astley', 'London', 'England', '123, sreet');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `email`
+--
+ALTER TABLE `email`
+  ADD KEY `id` (`id`);
+
+--
+-- Индексы таблицы `phone`
+--
+ALTER TABLE `phone`
+  ADD KEY `id` (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `email`
+--
+ALTER TABLE `email`
+  ADD CONSTRAINT `email_ibfk_1` FOREIGN KEY (`id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Ограничения внешнего ключа таблицы `phone`
+--
+ALTER TABLE `phone`
+  ADD CONSTRAINT `phone_ibfk_1` FOREIGN KEY (`id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
